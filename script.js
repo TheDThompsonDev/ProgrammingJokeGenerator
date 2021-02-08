a121b6faccf674fb2cf6e532f0a8c85ebdb0d045
dthompson_ftdr <danny.thompson@frontdoorhome.com>
2021-02-08 11:51:56 -0600

    Added comments to javascript file

--- script.js
+++ script.js
@@ -10,9 +10,17 @@ async function generateJokes() {
     "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
   )
   const data = await res.json();
+  //console logging the output to see what the API is returning
+  //we are returning 3 console.logs because the API sometimes
+  //returns data.joke with the joke and sometimes it returns
+  //data.setup and data.delivery
   console.log(data)
   console.log(data.joke)
   console.log(data.delivery)
+
+  //We are making an IF statement to see which way the API has decided to return the joke.
+  //This way we are prepared no matter how the joke is brought to us.
+  
   let joke = ""
   if (data.joke == undefined) {
     joke = `${data.setup} <br /> ${data.delivery}`
